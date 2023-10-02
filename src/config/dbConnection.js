5d00e8eb908b3c409f02b6afc7b47b62aa7e6468
Esteban Raffo <e.fraffo@gmail.com>
2023-10-02 11:40:07 -0300

    dbCon

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     try{
-        await mongoose.connect('mongodb+srv://efraffo:uRl0eN3R9Ld1cpNP@clustercoderbackend.k37s4qc.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect('')
         console.log("Base de datos conectada");
     }catch(error){
         console.log("Hubo un error al conectar a la base de datos: ", error.message);
