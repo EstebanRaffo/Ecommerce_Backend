5fd2b0a1fbfd2bcc22379a8b3300652c1cc6ef81
Esteban Raffo <e.fraffo@gmail.com>
2023-12-25 22:14:43 -0300

    Swagger Cart

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/", CartsController.getCarts);
 router.post("/", CartsController.createCart);
 router.get("/:cid", CartsController.getCartById);
-router.post("/:cid/product/:pid", authorize(["user","premium"]), CartsController.addProductToCart);
+router.post("/:cid/products/:pid", authorize(["user","premium"]), CartsController.addProductToCart);
 router.delete("/:cid/products/:pid", CartsController.deleteProduct);
 router.put("/:cid", CartsController.updateCart);
 router.put("/:cid/products/:pid", CartsController.updateProductQuantityInCart);
