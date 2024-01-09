65a898e82a088495c809d8d7ede4e8effb277f2f
Esteban Raffo <e.fraffo@gmail.com>
2024-01-08 22:09:45 -0300

    Test Products y Carts

--- test/ecommerce.test.js
+++ test/ecommerce.test.js
@@ -5,6 +5,7 @@ import { createHash } from "../src/utils.js";
 import { usersModel } from "../src/dao/mongo/models/users.model.js";
 import { config } from "../src/config/config.js";
 import { productsModel } from "../src/dao/mongo/models/products.model.js";
+import { cartsModel } from "../src/dao/mongo/models/carts.model.js";
 
 const requester = supertest(app);
 
@@ -67,7 +68,13 @@ describe("Testing Ecommerce", ()=>{
                     "https://i.postimg.cc/K8xyhkFx/notebook-4.png",
                     "https://i.postimg.cc/g2sbzzkR/notebook-5.jpg"]
         }
+        const updateData = {
+            "title": "Notebook Modificada TEST", 
+            "stock": 10,
+            "price": 100000
+        }
         let cookie;
+        let pid;
 
         before(async ()=>{
             const response = await requester.post("/api/sessions/login").send({
@@ -89,27 +96,74 @@ describe("Testing Ecommerce", ()=>{
             const response = await requester.post("/api/products/").send(mockProduct).set("Cookie",[`${cookie.name}=${cookie.value}`]);
             expect(response.status).to.be.equal(201);
             expect(response._body.data).to.have.property("_id");
+            pid = response._body.data._id;
         });
 
         it("Test Actualización de Producto -> PUT /api/products/:pid", async ()=>{
-            const response = await requester.put("/api/products/:pid").send({"title": "Notebook Modificada", "stock": 10}).set("Cookie",[`${cookie.name}=${cookie.value}`]);
-            console.log(response)
+            const response = await requester.put(`/api/products/${pid}`)
+            .send(updateData).set("Cookie",[`${cookie.name}=${cookie.value}`]);
+            expect(response.status).to.be.equal(201);
+            expect(response._body.message).to.be.equal("Producto actualizado exitosamente");
+            const { _id, title, stock, price } = response._body.data; 
+            expect(_id).to.be.equal(pid);
+            expect(title).to.be.equal(updateData.title);
+            expect(stock).to.be.equal(updateData.stock);
+            expect(price).to.be.equal(updateData.price);
         });
 
-        it("Test Eliminar Producto -> DELETE /api/products/:pid", ()=>{
-            
+        it("Test Eliminar Producto -> DELETE /api/products/:pid", async ()=>{
+            const response = await requester.delete(`/api/products/${pid}`).set("Cookie",[`${cookie.name}=${cookie.value}`]);
+            expect(response.status).to.be.equal(201);
+            expect(response._body.message).to.be.equal("Producto eliminado exitosamente");
+            const result = await requester.get(`/api/products/${pid}`);
+            expect(result.status).to.be.equal(200);
+            expect(result.text).to.be.equal("No se encontró el producto buscado o no existe");
         });
     });
 
     describe("Testing Carrito de Compras", ()=>{
-        it("Test Crear Carrito -> POST /api/carts/", ()=>{
+        let cookie;
+        let cid;
+        const pid = "651b65c6b8e03f977b791534";
+        const quantity_test = 10;
 
+        before(async ()=>{
+            const response = await requester.post("/api/sessions/login").send({
+                email: "test.user@test.com",
+                password: "abc123"
+            });
+            const cookieResult = response.header['set-cookie'][0];
+            cookie = {
+                name: cookieResult.split("=")[0],
+                value: cookieResult.split("=")[1]
+            };
         });
-        it("Test Agregar producto en el carrito -> POST /:cid/products/:pid", ()=>{
 
+        after(async ()=>{
+            await cartsModel.findByIdAndDelete(cid);
         });
-        it("Test Eliminar Producto del carrito -> DELETE /:cid/products/:pid", ()=>{
-            
+
+        it("Test Crear Carrito -> POST /api/carts/", async ()=>{
+            const response = await requester.post("/api/carts/").set("Cookie",[`${cookie.name}=${cookie.value}`]);
+            expect(response.status).to.be.equal(201);
+            expect(response._body.data).to.have.property("_id");
+            cid = response._body.data._id;
+        });
+        it("Test Agregar producto en el carrito -> POST /:cid/products/:pid", async ()=>{
+            const response = await requester.post(`/api/carts/${cid}/products/${pid}`).set("Cookie",[`${cookie.name}=${cookie.value}`]);
+            expect(response.status).to.be.equal(201);
+            const { _id, quantity } = response._body.data.products[0];
+            expect(_id._id).to.be.equal(pid);
+            expect(quantity).to.be.equal(1);
+        });
+        it("Test Actualizar cantidad del producto en el carrito -> PUT /:cid/products/:pid", async ()=>{
+            const response = await requester.put(`/api/carts/${cid}/products/${pid}`)
+            .send({"quantity": quantity_test}).set("Cookie",[`${cookie.name}=${cookie.value}`]);
+            expect(response.status).to.be.equal(201);
+            const { _id, quantity } = response._body.data.products[0];
+            console.log(response._body.data.products[0])
+            expect(_id._id).to.be.equal(pid);
+            expect(quantity).to.be.equal(quantity_test);            
         });
     });
 });
