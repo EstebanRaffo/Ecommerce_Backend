b3b59eba1027df9066a1b2a0cbf11e6b727c5a68
Esteban Raffo <e.fraffo@gmail.com>
2024-02-02 10:39:42 -0300

    formulario de pago

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { PaymentsService } from "../services/payments.service.js";
 import { CartsService } from "../services/carts.service.js";
 import { config } from "../config/config.js";
 import stripePackage from 'stripe';
@@ -7,26 +6,6 @@ import stripePackage from 'stripe';
 const stripe = stripePackage(config.stripe.secretKey);
 
 const router = Router();
-
-router.post("/payment-intents", async(req,res)=>{
-    try {
-        console.log(req.user)
-        const cid = req.user.cart.valueOf();
-        console.log(cid)
-        const cart = await CartsService.getCartById(cid);
-        const {products} = cart;
-        const purchase_amount = products.reduce((sum, product) => sum + (product.quantity * product._id.price), 0);
-        const paymentInfo = {
-            amount:purchase_amount,
-            currency: "ars"
-        };
-        const service = new PaymentsService();
-        const resultPaymentIntent = await service.createPaymentIntent(paymentInfo);
-        res.json({status:"success", payload:resultPaymentIntent});
-    } catch (error) {
-        return res.status(400).json({status:"error", error:error.message});
-    }
-});
 
 router.get('/checkout', (req, res) => {
     res.render('checkout', { stripePublicKey: config.stripe.publicKey });
