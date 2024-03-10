ea25a44af8d281358ebbc3e15671ce31a03b1793
Esteban Raffo <e.fraffo@gmail.com>
2024-03-10 16:13:54 -0300

    app

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"/public")));
 
 const httpServer = app.listen(port, ()=>logger.info(`Servidor escuchando en el puerto: ${port}`));
